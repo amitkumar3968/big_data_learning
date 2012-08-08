f2791a779abd08c91d8b43f2d3d20258ece9f1af
ahmed <zubayr.a@gmail.com>
2012-08-08 14:51:25 +0530

    Formated Code.

--- bigData/mapReduceCodes/WordCount/src/com/ahmed/mapreduce/WordCount.java
+++ bigData/mapReduceCodes/WordCount/src/com/ahmed/mapreduce/WordCount.java
@@ -15,67 +15,65 @@ import org.apache.hadoop.util.*;
 
 /**
  * @author Ahmed
- *
+ * 
  */
 
 public class WordCount {
 
-	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>
-	{
+	public static class Map extends MapReduceBase implements
+			Mapper<LongWritable, Text, Text, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
-		
+
 		@Override
 		public void map(LongWritable key, Text value,
 				OutputCollector<Text, IntWritable> output, Reporter reporter)
 				throws IOException {
-			String line = value.toString();			
+			String line = value.toString();
 			StringTokenizer tokens = new StringTokenizer(line);
-			
-			while(tokens.hasMoreTokens())
-			{
+
+			while (tokens.hasMoreTokens()) {
 				word.set(tokens.nextToken());
 				output.collect(word, one);
 			}
-			
+
 		}
-		
+
 	}
-	
-	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable>
-	{
+
+	public static class Reduce extends MapReduceBase implements
+			Reducer<Text, IntWritable, Text, IntWritable> {
 
 		@Override
 		public void reduce(Text key, Iterator<IntWritable> value,
-				OutputCollector<Text, IntWritable> outputCollector, Reporter reporter)
-				throws IOException {
+				OutputCollector<Text, IntWritable> outputCollector,
+				Reporter reporter) throws IOException {
 			int sum = 0;
-			while(value.hasNext())
-			{
+			while (value.hasNext()) {
 				sum = sum + value.next().get();
 			}
 			outputCollector.collect(key, new IntWritable(sum));
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		JobConf conf = new JobConf(WordCount.class);
 		conf.setJobName("WordCountProgram");
-		
+
 		conf.setOutputKeyClass(Text.class);
 		conf.setOutputValueClass(IntWritable.class);
-		
+
 		conf.setMapperClass(Map.class);
 		conf.setCombinerClass(Reduce.class);
 		conf.setReducerClass(Reduce.class);
-		
+
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
-		
+
 		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path (args[1]));
-		
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
 		JobClient.runJob(conf);
 
 	}
